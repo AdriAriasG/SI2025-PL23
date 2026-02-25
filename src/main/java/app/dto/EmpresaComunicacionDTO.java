79325a7057903420282ac436c6fc63e7f6efa44d
QraTz <qratz.otros@gmail.com>
2026-02-25 14:44:56 +0100

    Actualizacion y correccion del login

--- src/main/java/app/dto/EmpresaComunicacionDTO.java
+++ src/main/java/app/dto/EmpresaComunicacionDTO.java
@@ -9,4 +9,9 @@ public class EmpresaComunicacionDTO {
     public void setId(int id) { this.id = id; }
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
+
+    @Override
+    public String toString() {
+        return nombre; // Para que se vea el nombre en JComboBox/JList
+    }
 }
